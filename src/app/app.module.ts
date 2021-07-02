56ee341aac88a48eb99a69d11dfa81370a7584f3
Piotr Kuniniec <peter@digitalpurpose.com.au>
2021-07-03 09:41:12 +1000

    project start, new components: app-add-toys, app-view-toys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { WarningAlert } from './warning-alert/warning-alert.component';
-import { SuccessAlert } from './success-alert/success-alert.component';
-import { ToysComponent } from './toys/toys.component';
-import { ToysDetailsComponent } from './toys-details/toys-details.component';
+import { AddToysComponent } from './components/add-toys/add-toys.component';
+import { ViewToysComponent } from './components/view-toys/view-toys.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    WarningAlert,
-    SuccessAlert,
-    ToysComponent,
-    ToysDetailsComponent
+    AddToysComponent,
+    ViewToysComponent,
+
   ],
   imports: [
     BrowserModule,
